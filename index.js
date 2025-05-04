0d99392c6a9d61a67437f6fab37e9cc3b880e84b
Ranjitha <93441588+Ranjitha-Duraisamy27@users.noreply.github.com>
2025-05-04 20:13:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,4 +24,7 @@ app.post('/add', async (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
+});
+
